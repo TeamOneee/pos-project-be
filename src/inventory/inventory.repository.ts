2b358f59614ea942c3851f2ef5c8ee8031957718
nayraarr <zita.nayra.ardini@gmail.com>
2026-08-14 17:46:36 +0700

    refactor: remove legacy single-app src & stale artifacts

--- src/inventory/inventory.repository.ts
+++
@@ -1,8 +0,0 @@
-import { BaseRepository } from "src/common/repositories/base.repository";
-import { PrismaService } from "src/prisma/prisma.service";
-
-export class InventoryRepository extends BaseRepository {
-    constructor(prisma: PrismaService) {
-        super(prisma)
-    }
-}
--- src/outlets/outlets.repository.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaService } from "src/prisma/prisma.service";
-
-export class OutletsRepository {
-    constructor(private prisma: PrismaService) {}
-}
