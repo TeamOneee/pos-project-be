912822d2eec5654cff4456f350fca9269781a68e
saputt <sauki666indo@gmail.com>
2026-08-15 13:17:00 +0700

    feat(inventory): wire InventoryModule providers and barrel exports

--- libs/inventory/src/inventory.module.ts
+++ libs/inventory/src/inventory.module.ts
@@ -1,5 +1,28 @@
 import { Module } from '@nestjs/common';
+import { PlatformModule } from '@app/platform';
+import { CatalogModule } from '@app/catalog';
+import { TenantModule } from '@app/tenant';
+import { StockReservationPort } from './application/stock-reservation.port';
+import { StockReservationService } from './application/stock-reservation.service';
+import { InventoryQueryService } from './application/inventory-query.service';
+import { StockAdjustmentService } from './application/stock-adjustment.service';
+import { LowStockThresholdService } from './application/low-stock-threshold.service';
+import { StockMovementQueryService } from './application/stock-movement-query.service';
+import { OutletCatalogQueryService } from './application/outlet-catalog-query.service';
+import { InventoryController } from './web/inventory.controller';
+import { CatalogController } from './web/catalog.controller';
 
-// Stok per outlet, stock movement, StockReservationPort. Implementasi diisi bertahap.
-@Module({})
+@Module({
+  imports: [PlatformModule, CatalogModule, TenantModule],
+  controllers: [InventoryController, CatalogController],
+  providers: [
+    InventoryQueryService,
+    StockAdjustmentService,
+    LowStockThresholdService,
+    StockMovementQueryService,
+    OutletCatalogQueryService,
+    { provide: StockReservationPort, useClass: StockReservationService },
+  ],
+  exports: [StockReservationPort],
+})
 export class InventoryModule {}
